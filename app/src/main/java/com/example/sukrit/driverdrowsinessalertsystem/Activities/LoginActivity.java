6820aee6c00445ea9e90b9ed8715a61c60170b1b
Sukrit-Mehta <sukritmehta24@gmail.com>
2018-04-12 04:45:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sukrit/driverdrowsinessalertsystem/Activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.sukrit.driverdrowsinessalertsystem.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.sukrit.driverdrowsinessalertsystem.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
